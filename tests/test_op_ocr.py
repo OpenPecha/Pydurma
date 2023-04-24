492a8af1fa4f8631d865870d40d912b39518b10a
kaldan007 <kaldantenzin@gmail.com>
2023-04-24 20:36:20 +0530

    plain text vulgatizer added

--- tests/test_op_ocr.py
+++ tests/test_op_ocr.py
@@ -1,8 +1,10 @@
 import logging
+from pathlib import Path
 
 from openpecha.core.pecha import OpenPechaFS
 
 from vulgatizer.vulgatizer_op_ocr import VulgatizerOPTibOCR
+from vulgatizer.vulgatizer_plain_text import VulgatizerPlainText
 
 
 
@@ -13,22 +15,26 @@ from vulgatizer.vulgatizer_op_ocr import VulgatizerOPTibOCR
 #logger = logging.getLogger('FDMPVulgaligner')
 #logger.setLevel(logging.DEBUG)
 
-def test_merger():
-	op_output = OpenPechaFS("./data/I9B2646BE.opf")
+def test_vulgatizer_op():
+	op_output = OpenPechaFS("./data/vulgate/I9B2646BE.opf")
 	vulgatizer = VulgatizerOPTibOCR(op_output)
-	# vulgatizer.add_op_witness(OpenPechaFS("../test/opfs/I001/I001.opf"))
-	# vulgatizer.add_op_witness(OpenPechaFS("../test/opfs/I4DBEE949/I4DBEE949.opf"))
-	# vulgatizer.add_op_witness(OpenPechaFS("../test/opfs/I8B1FB7BB/I8B1FB7BB.opf"))
-	# vulgatizer.add_op_witness(OpenPechaFS("../test/opfs/IEA653111/IEA653111.opf"))
-	# vulgatizer.add_op_witness(OpenPechaFS("../test/opfs/I002/I002.opf"))
-	# vulgatizer.add_op_witness(OpenPechaFS("../test/opfs/I003/I003.opf"))
 
-	vulgatizer.add_op_witness(OpenPechaFS("./data/opfs/I001/I001.opf"))
-	vulgatizer.add_op_witness(OpenPechaFS("./data/opfs/I002/I002.opf"))
-	vulgatizer.add_op_witness(OpenPechaFS("./data/opfs/I4DBEE949/I4DBEE949.opf"))
-	vulgatizer.add_op_witness(OpenPechaFS("./data/opfs/I8B1FB7BB/I8B1FB7BB.opf"))
-	vulgatizer.add_op_witness(OpenPechaFS("./data/opfs/IEA653111/IEA653111.opf"))
-	vulgatizer.add_op_witness(OpenPechaFS("./data/opfs/I003/I003.opf"))
+	vulgatizer.add_op_witness(OpenPechaFS("./data/I0001/I0001.opf"))
+	vulgatizer.add_op_witness(OpenPechaFS("./data/I0002/I0002.opf"))
+	vulgatizer.add_op_witness(OpenPechaFS("./data/I0003/I0003.opf"))
+	vulgatizer.add_op_witness(OpenPechaFS("./data/I0004/I0004.opf"))
+	vulgatizer.add_op_witness(OpenPechaFS("./data/I0005/I0005.opf"))
+	
 	vulgatizer.create_vulgate()
 
-test_merger()
+
+def test_vulgatizer_plain_text():
+	op_output = OpenPechaFS("./data/vulgate/I9B2646B7.opf")
+	vulgatizer = VulgatizerPlainText(op_output)
+	vulgatizer.add_witness(Path("./data/I0001/I0001.opf/base/95E3.txt"))
+	vulgatizer.add_witness(Path("./data/I0002/I0002.opf/base/95E3.txt"))
+	vulgatizer.add_witness(Path("./data/I0003/I0003.opf/base/95E3.txt"))
+	vulgatizer.add_witness(Path("./data/I0004/I0004.opf/base/95E3.txt"))
+	vulgatizer.add_witness(Path("./data/I0005/I0005.opf/base/95E3.txt"))
+	
+	vulgatizer.create_vulgate()
