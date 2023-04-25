294f0832ee329c51f22f1e7eb4e1ca5b74aa1585
kaldan007 <kaldantenzin@gmail.com>
2023-04-25 12:04:07 +0530

    resource files shipped

--- src/vulgatizer/config.py
+++ src/vulgatizer/config.py
@@ -1,1 +1,7 @@
-BO_SYLLIST_PATH = './res/bo/syllist.txt'
+from pathlib import Path
+
+ROOT_PATH = Path(__file__).resolve().parent
+
+BO_SYLLIST_PATH = ROOT_PATH / 'res' / 'bo' / 'syllist.txt'
+
+assert BO_SYLLIST_PATH.exists()
